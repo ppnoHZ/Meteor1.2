1e5aa7b1e906e38b1b888302b6e907285920619d
zhoudd-mac <zhoudd@stark.tm>
2015-09-30 15:37:08 +0800

    重写错误提示消息。

--- autoformDemo/lib/0_schemas.js
+++ autoformDemo/lib/0_schemas.js
@@ -10,11 +10,29 @@
                     //这里可以做判断，判断语言。
                     return '标题'
                 },
-    2，
+ 2，
  *
  */
 
-
+/*
+ type: Match.Any,                                                                                                     // 15
+ label: Match.Optional(Match.OneOf(String, Function)),                                                                // 16
+ optional: Match.Optional(Match.OneOf(Boolean, Function)),                                                            // 17
+ min: Match.Optional(Match.OneOf(Number, Date, Function)),                                                            // 18
+ max: Match.Optional(Match.OneOf(Number, Date, Function)),                                                            // 19
+ minCount: Match.Optional(Match.OneOf(Number, Function)),                                                             // 20
+ maxCount: Match.Optional(Match.OneOf(Number, Function)),                                                             // 21
+ allowedValues: Match.Optional(Match.OneOf([Match.Any], Function)),                                                   // 22
+ decimal: Match.Optional(Boolean),                                                                                    // 23
+ exclusiveMax: Match.Optional(Boolean),                                                                               // 24
+ exclusiveMin: Match.Optional(Boolean),                                                                               // 25
+ regEx: Match.Optional(Match.OneOf(RegExp, [RegExp])),                                                                // 26
+ custom: Match.Optional(Function),                                                                                    // 27
+ blackbox: Match.Optional(Boolean),                                                                                   // 28
+ autoValue: Match.Optional(Function),                                                                                 // 29
+ defaultValue: Match.Optional(Match.Any),                                                                             // 30
+ trim: Match.Optional(Boolean)
+ */
 
 Schemas = {};
 
@@ -24,24 +42,61 @@ Schemas.Books = new SimpleSchema(
             type: String,
             label: function () {
                 //这里可以做判断，判断语言。
-                return '标题'
+                return '标题';
             },
-            max: 200
+            max: 200,
+            custom: function () {
+                if (Meteor.isClient && this.isSet) {
+                    if (this.value == 'zdd') {
+                        Schemas.Books.namedContext("createBooksForm").addInvalidKeys([{name: "title", type: "myErrorType"}]);
+                    }
+
+                }
+            }
         },
         author: {
             type: String,
-            label: "作者"
-        },
+            label: "作者",
+            custom: function () {
+                if (Meteor.isClient && this.isSet) {
+                    if (this.value == 'zdd') {
+                        console.log('重复');
+                        //Schemas.Books.namedContext("createBooksForm").addInvalidKeys([{name: "author", type: "myErrorType"}]);
+                        ssContext.addInvalidKeys([{name: "author", type: "myErrorType"}]);
+
+
+                        var ik = ssContext.invalidKeys();
+                        ik = _.map(ik, function (o) {
+                            return _.extend({message: ssContext.keyErrorMessage(o.name)}, o);
+                        });
+
+                        console.log(JSON.stringify(ik));
+                        //判断是指定key是否验证通过
+                        console.log(ssContext.keyIsInvalid('author'));
+                        //获取指定key的错误消息。
+                        console.log(ssContext.keyErrorMessage('author'));
+                        //清除 content对象。所有的错误消息将被清除。
+                        ssContext.resetValidation();
+
+
+                    }
+
+                }
+            }
+        }
+        ,
         copies: {
             type: Number,
             label: "Number of copies",
             min: 0
-        },
+        }
+        ,
         lastCheckedOut: {
             type: Date,
             label: "Last date this book was checked out",
             optional: true
-        },
+        }
+        ,
         summary: {
             type: String,
             //label: "Brief summary",
@@ -50,8 +105,10 @@ Schemas.Books = new SimpleSchema(
         }
     }
 );
+
+//单独配置key的显示信息。
 Schemas.Books.labels({
-    summary:'Brief'
+    summary: 'Brief'
 })
 
 //
@@ -125,17 +182,19 @@ Schemas.User = new SimpleSchema({
 });
 
 
-/* 提示消息的占位符
-[label] will be replaced with the field label
-[min] will be replaced with the minimum allowed value (string length, number, or date)
-[max] will be replaced with the maximum allowed value (string length, number, or date)
-[minCount] will be replaced with the minimum array count
-[maxCount] will be replaced with the maximum array count
-[value] will be replaced with the value that was provided to save but was invalid (not available for all error types)
-[type] will be replaced with the expected type; useful for the expectedConstructor error type
-*/
+/*
+ * 提示消息的占位符
+ [label] will be replaced with the field label
+ [min] will be replaced with the minimum allowed value (string length, number, or date)
+ [max] will be replaced with the maximum allowed value (string length, number, or date)
+ [minCount] will be replaced with the minimum array count
+ [maxCount] will be replaced with the maximum array count
+ [value] will be replaced with the value that was provided to save but was invalid (not available for all error types)
+ [type] will be replaced with the expected type; useful for the expectedConstructor error type
+ */
 SimpleSchema.messages({
-    required:"[label] 不能为空！",
+    myErrorType: '[label]重复',
+    required: "[label] 不能为空！",
     minString: "[label] must be at least [min] characters",
     maxString: "[label] cannot exceed [max] characters",
     minNumber: "[label] must be at least [min]",
@@ -167,7 +226,7 @@ SimpleSchema.messages({
     ],
     keyNotInSchema: "[key] is not allowed by the schema"
 });
-//var ssContext= Schemas.Books.namedContext('userForm');
+var ssContext = Schemas.Books.namedContext('userForm');
 //
 //ssContext.validate(,);
 

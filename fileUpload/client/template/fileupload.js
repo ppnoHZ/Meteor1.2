9c912d45c709164f24411f511f2471ff8306322c
zhoudd-mac <zhoudd@stark.tm>
2015-10-12 17:28:31 +0800

    添加文件上传Demo

---
+++ fileUpload/client/template/fileupload.js
@@ -0,0 +1,28 @@
+if (Meteor.isClient) {
+
+    Template.fileupload.events({
+        'change #file': function (event, template) {
+            FS.Utility.eachFile(event, function (file) {
+                Images.insert(file, function (err, fileobj) {
+                    if (err) {
+
+                    } else {
+                        var userID = Meteor.user();
+                        var imagesURL = {
+                            'profile.image': '/cfs/files/images/' + fileobj._id
+                        }
+                    }
+                    Meteor.users.update(userID, {$set: imagesURL});
+                })
+            })
+        },
+        'click input[type="submit"]': function () {
+            //访问选择的图片
+            var file = $('#file').get(0).files[0];
+            console.log('Upload result: ', file);
+        }
+    });
+}
+
+
+

8bfbcf7a92bfdc5c6e32a3367229957b5dc3e67d
zhoudd-mac <zhoudd@stark.tm>
2015-10-26 10:36:40 +0800

    更新readme.md

---
+++ autoformDemo/client/lib/test.js
@@ -0,0 +1,14 @@
+/*
+MySchema = new SimpleSchema({
+    addresses: {
+        type: [Object],
+        minCount: 1,
+        maxCount: 4
+    },
+    "addresses.$.street": {
+        type: String
+    },
+    "addresses.$.city": {
+        type: String
+    }
+});*/
